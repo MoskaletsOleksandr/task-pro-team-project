c573d8408184d9d7ce6728456c7ff4cb83486c1b
Олександр <moskaletzsanych@gmail.com>
2023-08-25 02:42:04 +0300

    variables-fonts-added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,19 @@
+import { useEffect, useState } from 'react';
+import GlobalStyles from './GlobalStyles';
+import { TestThemeBtn } from './TestThemeBtn/TestThemeBtn';
+
 export const App = () => {
+  const [theme, setTheme] = useState('dark');
+
+  useEffect(() => {
+    document.body.classList.add(theme);
+    return () => {
+      document.body.classList.remove(theme);
+    };
+  }, [theme]);
+  const handleThemeChange = color => {
+    setTheme(color);
+  };
   return (
     <div
       style={{
@@ -7,10 +22,25 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      Test
+      <GlobalStyles />
+      <TestThemeBtn
+        onClick={() => {
+          handleThemeChange('violet');
+        }}
+      >
+        Тема violet
+      </TestThemeBtn>
+      <TestThemeBtn
+        onClick={() => {
+          handleThemeChange('dark');
+        }}
+      >
+        Тема dark
+      </TestThemeBtn>
     </div>
   );
 };
