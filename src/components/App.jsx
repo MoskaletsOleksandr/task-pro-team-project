6d98763273303e85c81035af069f0db5d26e9068
Олександр <moskaletzsanych@gmail.com>
2023-08-26 19:32:32 +0300

    GlobalStyles-added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
 import GlobalStyles from './GlobalStyles';
-import RegistrationForm from './registrationForm/RegistrationForm';
+// import RegistrationForm from './registrationForm/RegistrationForm';
 import { Routes, Route } from 'react-router-dom';
 import WelcomePage from './WelcomePage/WelcomePage';
 import AuthPage from './AuthPage/AuthPage';
 
 export const App = () => {
   return (
-     //<RegistrationForm/>
-    <Routes>
-      <Route index element={<WelcomePage />} />
-      <Route path="/welcome" element={<WelcomePage />} />
-      <Route path="/auth/:id" element={<AuthPage />} />
-    </Routes>
+    <>
+      <GlobalStyles />
+      {/* <RegistrationForm /> */}
+      <Routes>
+        <Route index element={<WelcomePage />} />
+        <Route path="/welcome" element={<WelcomePage />} />
+        <Route path="/auth/:id" element={<AuthPage />} />
+      </Routes>
+    </>
   );
 };
