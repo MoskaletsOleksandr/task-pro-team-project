048046a123c694fbc68f76f95499c427ccfd665a
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-08-27 15:26:29 +0300

    form autocomplete

--- src/components/registrationForm/authInput.jsx
+++ src/components/registrationForm/authInput.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { ErrorMessage } from 'formik';
 import AuthTextError from './authTextError';
-import { AuthLabelSyled, Input } from './RegistrationFormStyled';
+import {  Input } from './RegistrationFormStyled';
+import { AuthLabelStyled } from './RegistrationFormStyled';
 
 function authInput(props) {
   const { placeholder, name, ...rest } = props;
   return (
     <div className="form-control">
-      <AuthLabelSyled>
+      <AuthLabelStyled>
         <Input name={name} placeholder={placeholder} {...rest} />
-      </AuthLabelSyled>
+      </AuthLabelStyled>
       <ErrorMessage name={name} component={AuthTextError} />
     </div>
   );
