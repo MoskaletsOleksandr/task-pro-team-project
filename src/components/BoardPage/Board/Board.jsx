546250d5bae0300b83c44ee6f71da6e20dfe0e45
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-08-28 10:45:32 +0200

    ScreenPage updated

--- src/components/BoardPage/Board/Board.jsx
+++ src/components/BoardPage/Board/Board.jsx
@@ -4,20 +4,17 @@ import Column from '../Column/Column';
 import { DashboardContainer, ColumnWrapper } from './Board.styled';
 
 const MainDashboard = () => {
-    return (
-        <DashboardContainer>
-            <div style={{ flexGrow: 1, display: 'flex' }}>
-                {currentBoardForScreensPage.columns.map(column => (
-                    <ColumnWrapper key={column._id}>
-                        <Column
-                            title={column.title}
-                            tasks={column.tasks}
-                        />
-                    </ColumnWrapper>
-                ))}
-            </div>
-        </DashboardContainer>
-    );
+  return (
+    <DashboardContainer>
+      <div style={{ flexGrow: 1, display: 'flex' }}>
+        {currentBoardForScreensPage.columns.map(column => (
+          <ColumnWrapper key={column._id}>
+            <Column title={column.title} tasks={column.tasks} />
+          </ColumnWrapper>
+        ))}
+      </div>
+    </DashboardContainer>
+  );
 };
 
 export default MainDashboard;
