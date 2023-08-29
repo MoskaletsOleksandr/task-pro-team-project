2591c4a99ae3bc79749b23ea1e6255e8a510e848
Oleksii Leschenko <112614267+DJlabuh@users.noreply.github.com>
2023-08-29 14:22:45 +0300

    Add EditUser

--- src/components/ModalEditProfile/ModalEditProfile.jsx
+++ src/components/ModalEditProfile/ModalEditProfile.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Modal from 'components/Modal';
 import ModalTitle from 'components/ModalTitle';
+import EditUser from 'components/EditUser';
 
 const ModalEditProfile = ({ closeModal, isOpen }) => {
   return (
-    <Modal onClose={closeModal} isOpen={isOpen}>
+    <Modal onClose={closeModal} isOpen={isOpen} custom={true}>
       <ModalTitle>Edit profile</ModalTitle>
+      <EditUser onClose={closeModal} />
     </Modal>
   );
 };
