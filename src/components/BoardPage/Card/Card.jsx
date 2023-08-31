33eb6bee53272eb11b135af79081b72fbc4e0326
Олександр <moskaletzsanych@gmail.com>
2023-08-31 19:38:09 +0300

    fix

--- src/components/BoardPage/Card/Card.jsx
+++ src/components/BoardPage/Card/Card.jsx
@@ -1,5 +1,4 @@
-import React from // ,{ useState }
-  'react';
+import React from 'react'; // ,{ useState }
 import LinesEllipsis from 'react-lines-ellipsis';
 import { currentBoardForScreensPage } from '../../../fakeData/fakeData';
 import {
@@ -21,14 +20,13 @@ import {
   PriorityInfo,
   PopUpMenu,
   Backdrop,
-  BackDropHiden
-  BellIcon
+  BackDropHiden,
+  BellIcon,
 } from '../Card/Card.styled';
 import CustomPopUpItem from '../PopUp/PopUp';
 import sprite from '../../../images/sprite.svg';
 
 const TaskCard = ({ taskId, togglePopUpMenu, isPopupOpen }) => {
-
   const today = new Date().toISOString().slice(0, 10);
 
   const selectedTask = currentBoardForScreensPage.columns
@@ -111,15 +109,15 @@ const TaskCard = ({ taskId, togglePopUpMenu, isPopupOpen }) => {
 
       {isPopupOpen && (
         // Vit
-       <> 
-        <BackDropHiden onClick={() => togglePopUpMenu(taskId)}>
-           </BackDropHiden>
-        <Backdrop
-          backgroundColor={priorityBorderColor}
-          onClick={() => togglePopUpMenu(taskId)}
-        />
-       </>
-       
+        <>
+          <BackDropHiden
+            onClick={() => togglePopUpMenu(taskId)}
+          ></BackDropHiden>
+          <Backdrop
+            backgroundColor={priorityBorderColor}
+            onClick={() => togglePopUpMenu(taskId)}
+          />
+        </>
       )}
       {isPopupOpen && (
         <PopUpMenu>
