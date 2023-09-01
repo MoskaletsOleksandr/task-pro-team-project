58901caa86c3c7504bdf667306211275487aee70
Олександр <moskaletzsanych@gmail.com>
2023-09-02 01:34:36 +0300

    Update ScreenPage.jsx

--- src/pages/ScreenPage/ScreenPage.jsx
+++ src/pages/ScreenPage/ScreenPage.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import NewDashboard from 'components/NewDashboard/NewDashboard';
 import MainDashboard from 'components/MainDashboard/MainDashboard';
+import { useSelector } from 'react-redux';
 
 // import { currentBoardForScreensPage } from '../../fakeData/fakeData';
 // import Column from '../../components/BoardPage/Column/Column';
 // import Board from '../../components/BoardPage/Board/Board';
 
 const ScreenPage = () => {
-  const isVisibleMainDashboard = true;
+  const currentBoard = useSelector(state => state.boards.currentBoard);
+  // const isVisibleMainDashboard = true;
   return (
     <div>
-      {isVisibleMainDashboard ? <MainDashboard /> : <NewDashboard />}
+      {currentBoard ? <MainDashboard /> : <NewDashboard />}
       {/* <Board>
         {currentBoardForScreensPage.columns.map(column => (
           <Column key={column._id} title={column.title} tasks={column.tasks} />
