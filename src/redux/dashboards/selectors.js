39ba2461347454af404c694b4ba3412cd0c39f97
Vitalii Ladanivskyi <ladanivskyy@gmail.com>
2023-09-02 14:02:18 +0200

    Render NewDashbord and MainDashboard

--- src/redux/dashboards/selectors.js
+++ src/redux/dashboards/selectors.js
@@ -1,1 +1,5 @@
 export const selectAllBoards = state => state.boards.info;
+
+export const selectBoard = state => state.boards;
+
+export const selectCurrentBoard = state => state.boards.currentBoard;
