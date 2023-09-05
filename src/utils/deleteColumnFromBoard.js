bbaf370c66373ff7d6103f3b4f75df5cf0f1c56c
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-05 15:01:11 +0300

    Update deleteColumnFromBoard.js

--- src/utils/deleteColumnFromBoard.js
+++ src/utils/deleteColumnFromBoard.js
@@ -1,5 +1,3 @@
-import { toast } from "react-hot-toast";
-
 // export const deleteColumnFromBoard = (board, deletedColumn) => {
 //   board.columns = board.columns.filter(column => column._id !== deletedColumn._id);
 
@@ -7,28 +5,13 @@ import { toast } from "react-hot-toast";
 // };
 // оновлений без мутації
 
-// export const deleteColumnFromBoard = (board, deletedColumn) => {
-//   const updatedBoard = JSON.parse(JSON.stringify(board));
-
-//   updatedBoard.columns = updatedBoard.columns.filter(
-//     column => column._id !== deletedColumn._id
-//   );
-
-//   return updatedBoard;
-// };
-
 export const deleteColumnFromBoard = (board, deletedColumn) => {
-  try {
-    const updatedBoard = JSON.parse(JSON.stringify(board));
+  const updatedBoard = JSON.parse(JSON.stringify(board));
 
-    updatedBoard.columns = updatedBoard.columns.filter(
-      column => column._id !== deletedColumn._id
-    );
+  updatedBoard.columns = updatedBoard.columns.filter(
+    column => column._id !== deletedColumn._id
+  );
 
-    toast.success('Column deleted successfully from the board.');
-    return updatedBoard;
-  } catch (error) {
-    toast.error('Error deleting column from the board.');
-    return board; // Повертаємо оригінальну дошку у випадку помилки.
-  }
+  return updatedBoard;
 };
+
