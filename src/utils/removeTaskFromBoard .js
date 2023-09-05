38f971dfa16e05c0e3e8121fcdade0f77db61735
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-05 15:01:21 +0300

    Update removeTaskFromBoard .js

--- src/utils/removeTaskFromBoard .js
+++ src/utils/removeTaskFromBoard .js
@@ -1,4 +1,3 @@
-import { toast } from "react-hot-toast";
 // export const removeTaskFromBoard = (board, task) => {
 //   const columnId = task.columnId;
 
@@ -10,46 +9,22 @@ import { toast } from "react-hot-toast";
 
 // without mutation
 
-// export const removeTaskFromBoard = (board, task) => {
-//   const columnId = task.columnId;
-
-//   const updatedBoard = JSON.parse(JSON.stringify(board));
-
-//   const columnIndex = updatedBoard.columns.findIndex(
-//     col => col._id === columnId
-//   );
-
-//   if (columnIndex !== -1) {
-//     const column = updatedBoard.columns[columnIndex];
-//     column.tasks = column.tasks.filter(t => t._id !== task._id);
-//   } else {
-//     console.error(`Column with ID ${columnId} not found.`);
-//   }
-
-//   return updatedBoard;
-// };
-
 export const removeTaskFromBoard = (board, task) => {
-  try {
-    const columnId = task.columnId;
+  const columnId = task.columnId;
 
-    const updatedBoard = JSON.parse(JSON.stringify(board));
+  const updatedBoard = JSON.parse(JSON.stringify(board));
 
-    const columnIndex = updatedBoard.columns.findIndex(
-      col => col._id === columnId
-    );
+  const columnIndex = updatedBoard.columns.findIndex(
+    col => col._id === columnId
+  );
 
-    if (columnIndex !== -1) {
-      const column = updatedBoard.columns[columnIndex];
-      column.tasks = column.tasks.filter(t => t._id !== task._id);
-      toast.success('Task removed successfully from the board.');
-    } else {
-      throw new Error(`Column with ID ${columnId} not found.`);
-    }
-
-    return updatedBoard;
-  } catch (error) {
-    toast.error('Error removing task from the board.');
-    return board;
+  if (columnIndex !== -1) {
+    const column = updatedBoard.columns[columnIndex];
+    column.tasks = column.tasks.filter(t => t._id !== task._id);
+  } else {
+    console.error(`Column with ID ${columnId} not found.`);
   }
+
+  return updatedBoard;
 };
+
