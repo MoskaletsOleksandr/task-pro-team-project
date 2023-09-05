15e7611ffc8dbcf049e8e20fea5a6f5fd11b3fd2
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-05 15:01:08 +0300

    Update addTaskToBoard.js

--- src/utils/addTaskToBoard.js
+++ src/utils/addTaskToBoard.js
@@ -1,5 +1,3 @@
-import { toast } from "react-hot-toast";
-
 // export const addTaskToBoard = (board, task) => {
 //   const columnId = task.columnId;
 
@@ -11,35 +9,13 @@ import { toast } from "react-hot-toast";
 
 // оновлений без мутації
 
-// export const addTaskToBoard = (board, task) => {
-//   const columnId = task.columnId;
-
-//   const updatedBoard = JSON.parse(JSON.stringify(board));
-
-//   const column = updatedBoard.columns.find(col => col._id === columnId);
-//   column.tasks.push(task);
-
-//   return updatedBoard;
-// };
-
 export const addTaskToBoard = (board, task) => {
-  try {
-    const columnId = task.columnId;
+  const columnId = task.columnId;
 
-    const updatedBoard = JSON.parse(JSON.stringify(board));
+  const updatedBoard = JSON.parse(JSON.stringify(board));
 
-    const column = updatedBoard.columns.find(col => col._id === columnId);
+  const column = updatedBoard.columns.find(col => col._id === columnId);
+  column.tasks.push(task);
 
-    if (column) {
-      column.tasks.push(task);
-      toast.success('Task added successfully to the board.');
-    } else {
-      throw new Error(`Column with ID ${columnId} not found.`);
-    }
-
-    return updatedBoard;
-  } catch (error) {
-    toast.error('Error adding task to the board.');
-    return board; 
-  }
+  return updatedBoard;
 };
