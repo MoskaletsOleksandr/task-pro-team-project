6f086fd8f25393651ada5362f860a201d1ed9880
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-05 15:01:24 +0300

    Update updateColumnTitleInBoard.js

--- src/utils/updateColumnTitleInBoard.js
+++ src/utils/updateColumnTitleInBoard.js
@@ -16,22 +16,6 @@ import { toast } from "react-hot-toast";
 
 // without mutation
 
-// export const updateColumnTitleInBoard = (board, updatedColumn) => {
-//   const updatedBoard = JSON.parse(JSON.stringify(board)); // Клонуємо об'єкт дошки
-
-//   const columnIndex = updatedBoard.columns.findIndex(
-//     column => column._id === updatedColumn._id
-//   );
-
-//   if (columnIndex !== -1) {
-//     updatedBoard.columns[columnIndex].title = updatedColumn.title;
-//   } else {
-//     console.error(`Column with ID ${updatedColumn._id} not found.`);
-//   }
-
-//   return updatedBoard; // Повертаємо оновлену копію об'єкта дошки
-// };
-
 export const updateColumnTitleInBoard = (board, updatedColumn) => {
   try {
     const updatedBoard = JSON.parse(JSON.stringify(board)); 
@@ -41,15 +25,20 @@ export const updateColumnTitleInBoard = (board, updatedColumn) => {
     );
 
     if (columnIndex !== -1) {
-      updatedBoard.columns[columnIndex].title = updatedColumn.title;
-      toast.success('Column title updated successfully');
-    } else {
-      throw new Error(`Column with ID ${updatedColumn._id} not found`);
-    }
+      if (!updatedColumn.title) {
 
-    return updatedBoard;
+        toast.error('Column title is required'); 
+      } else {
+        updatedBoard.columns[columnIndex].title = updatedColumn.title;
+      }
+    } else {
+      toast.error('Column not found'); 
+    }
+  
+    return updatedBoard; 
+
   } catch (error) {
-    toast.error('Error updating column title');
+    toast.error('Error updating column title'); 
     return board;
   }
 };
