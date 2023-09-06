864ae0388bd3d5ab17c6078841ed422a022eec7d
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-09-06 10:46:33 +0300

    Update notification

--- src/utils/placeTasksInColumns.js
+++ src/utils/placeTasksInColumns.js
@@ -1,4 +1,3 @@
-import { toast } from "react-hot-toast";
 // export const placeTasksInColumns = (board, tasks) => {
 //   const columnTasksMap = {};
 //   тут мутація
@@ -21,50 +20,24 @@ import { toast } from "react-hot-toast";
 
 // оновлений без мутації
 
-// export const placeTasksInColumns = (board, tasks) => {
-//   const updatedBoard = JSON.parse(JSON.stringify(board));
-
-//   const columnTasksMap = {};
-//   updatedBoard.columns.forEach(column => {
-//     columnTasksMap[column._id] = [];
-//   });
-
-//   tasks.forEach(task => {
-//     if (columnTasksMap.hasOwnProperty(task.columnId)) {
-//       columnTasksMap[task.columnId].push(task);
-//     }
-//   });
-
-//   updatedBoard.columns.forEach(column => {
-//     column.tasks = columnTasksMap[column._id];
-//   });
-
-//   return updatedBoard;
-// };
-
 export const placeTasksInColumns = (board, tasks) => {
-  try {
-    const updatedBoard = JSON.parse(JSON.stringify(board));
+  const updatedBoard = JSON.parse(JSON.stringify(board));
 
-    const columnTasksMap = {};
-    updatedBoard.columns.forEach(column => {
-      columnTasksMap[column._id] = [];
-    });
+  const columnTasksMap = {};
+  updatedBoard.columns.forEach(column => {
+    columnTasksMap[column._id] = [];
+  });
 
-    tasks.forEach(task => {
-      if (columnTasksMap.hasOwnProperty(task.columnId)) {
-        columnTasksMap[task.columnId].push(task);
-      }
-    });
+  tasks.forEach(task => {
+    if (columnTasksMap.hasOwnProperty(task.columnId)) {
+      columnTasksMap[task.columnId].push(task);
+    }
+  });
 
-    updatedBoard.columns.forEach(column => {
-      column.tasks = columnTasksMap[column._id];
-    });
+  updatedBoard.columns.forEach(column => {
+    column.tasks = columnTasksMap[column._id];
+  });
 
-    toast.success('Tasks placed successfully in the columns');
-    return updatedBoard;
-  } catch (error) {
-    toast.error('Error placing tasks in the columns');
-    return board;
-  }
+  return updatedBoard;
 };
+
